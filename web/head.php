d515b3aa2cd19282b662014da5de58c9b39de456
B3none <ablackham2000@gmail.com>
2019-04-28 21:37:13 +0100

    alter header

--- web/head.php
+++ web/head.php
@@ -1,7 +1,5 @@
-<?php
-require(__DIR__ . "/../app/config.php");
+<?php require(__DIR__ . "/../app/config.php"); ?>
 
-echo '        
-    <a class="text-white" href="./">
-        <h1 class="text-center"><img class="img-fluid" src="'.$logo.'" style="width: 100px; margin-right: 15px;">'.$site_name.'</h1>
-    </a>';
+<a class="text-white" href="./">
+    <h1 class="text-center"><?= $site_name ?></h1>
+</a>
