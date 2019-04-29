d305411c7fb6c0286875ef0667fedc29f9da4403
B3none <ablackham2000@gmail.com>
2019-04-29 09:30:27 +0100

    update head

--- web/head.php
+++ web/head.php
@@ -1,6 +1,6 @@
 <?php require(__DIR__ . "/../app/config.php"); ?>
 
-<a class="text-white" href="./">
+<a class="text-white" href="https://redlinecs.net">
     <h1 class="text-center">
         <?= $site_name ?>
     </h1>
