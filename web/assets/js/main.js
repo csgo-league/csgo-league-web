28067e7946547fe262979fc1af286d1f450ed236
B3none <ablackham2000@gmail.com>
2019-04-30 11:31:11 +0100

    added am pm

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,10 +1,17 @@
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  return hours + ':' + minutes + ' ' + ampm;
+}
+
 $(document).ready(function() {
   $('.match').each(function(i, el) {
     el = $(el);
     var matchDate = $(el.find('.timestamp')[0]);
     var timestamp = parseInt(matchDate.prop('innerHTML'));
     var date = new Date(timestamp * 1000);
-    matchDate.prop('innerHTML', date.toLocaleDateString() + ' ' + date.getHours() + ':' + date.getMinutes());
+    matchDate.prop('innerHTML', date.toLocaleDateString() + ' ' + formatAMPM(date));
     el.removeClass('d-none');
   });
 });
