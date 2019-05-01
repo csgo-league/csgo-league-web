0d745db0dbac6da1a6a26241173b6520302cf259
B3none <ablackham2000@gmail.com>
2019-05-01 14:15:07 +0100

    update router and add matchcontroller as well as profilecontroller

--- app/Router.php
+++ app/Router.php
@@ -1,9 +1,5 @@
 <?php
 
-use B3none\VoidRealityGamingRankme\Controllers\ErrorController;
-use B3none\VoidRealityGamingRankme\Controllers\LeaderboardController;
-use B3none\VoidRealityGamingRankme\Controllers\ProfileController;
-use B3none\VoidRealityGamingRankme\Controllers\SearchController;
 use Pecee\Http\Middleware\Exceptions\TokenMismatchException;
 use Pecee\Http\Request;
 use Pecee\SimpleRouter\Exceptions\HttpException;
@@ -19,9 +15,6 @@ class Router
     {
         // Load all of the necessary routes
         $this->registerRoutes();
-
-        // Set the default namespace for controllers.
-//        SimpleRouter::setDefaultNamespace('\B3none\VoidRealityGamingRankme\Controllers');
 
         // Start the routing
         try {
@@ -43,24 +36,21 @@ class Router
     {
         // Redirect to the leaderboard homepage.
         $homeRedirects = [
-            '/',
-            '/home',
+            '/match',
             '/profile'
         ];
         foreach ($homeRedirects as $homeRedirect) {
             SimpleRouter::get($homeRedirect, function () {
-                response()->redirect('/leaderboard');
+                response()->redirect('/');
             });
         }
 
-        SimpleRouter::controller('/leaderboard', LeaderboardController::class);
+        SimpleRouter::controller('/matches', MatchesController::class);
+        SimpleRouter::get('/match/{matchId}', MatchController::class . '@getMatch');
+        SimpleRouter::get('/profile/{steamId}', ProfileController::class . '@getPlayerProfile');
 
-        SimpleRouter::group(['defaultParameterRegex' => '[\w\(%|:|_)]+'], function() {
-            SimpleRouter::get('/profile/{steamId}', ProfileController::class . '@getPlayerProfile');
-            SimpleRouter::get('/errors/{errors}', ErrorController::class . '@returnError');
-        });
-
-        SimpleRouter::controller('/search', SearchController::class);
+//        SimpleRouter::get('/errors/{errors}', ErrorController::class . '@returnError');
+//        SimpleRouter::controller('/search', SearchController::class);
 
         // Anything that's not registered fallback to the homepage.
         SimpleRouter::error(function(Request $request, \Exception $exception) {
