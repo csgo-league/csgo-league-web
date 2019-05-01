5fde37b640b833889505649159c8d7726583081a
B3none <ablackham2000@gmail.com>
2019-05-02 00:31:19 +0100

    started match model, updated composer

---
+++ src/Models/MatchModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Redline\League\Models;
+
+class MatchModel
+{
+    protected $matchId;
+    protected $name;
+    protected $steamId;
+    protected $team;
+    protected $alive;
+    protected $ping;
+    protected $account;
+    protected $kills;
+    protected $assists;
+    protected $deaths;
+    protected $mvps;
+    protected $score;
+}
