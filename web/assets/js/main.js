41fd1561e59c464fd401e7d9db018f25e279cf9b
B3none <ablackham2000@gmail.com>
2019-05-01 12:27:26 +0100

    update main.js again for ErkiH

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,9 +1,3 @@
-function formatAMPM(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  return hours + ':' + minutes + (hours >= 12 ? 'pm' : 'am');
-}
-
 $(document).ready(function() {
   var mobilecheck = function() {
     var check = false;
@@ -24,7 +18,7 @@ $(document).ready(function() {
     var matchDate = $(el.find('.timestamp')[0]);
     var timestamp = parseInt(matchDate.prop('innerHTML'));
     var date = new Date(timestamp * 1000);
-    matchDate.prop('innerHTML', date.toLocaleDateString() + ' ' + formatAMPM(date));
+    matchDate.prop('innerHTML', date.toLocaleDateString() + ' ' + date.getHours() + ':' + date.getMinutes());
     el.removeClass('d-none');
   });
 
