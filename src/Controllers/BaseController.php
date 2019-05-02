9d6c52607148e3fbaa9c4ca1e27c53cb81ca1c35
B3none <ablackham2000@gmail.com>
2019-05-02 10:39:27 +0100

    added basecontroller

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -5,10 +5,17 @@ namespace Redline\League\Controllers;
 
 class BaseController
 {
+    /**
+     * @var \Twig\Environment
+     */
     protected $twig;
 
     public function __construct()
     {
-        $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../src/Views');
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../Views');
+
+        $this->twig = new \Twig\Environment($loader, [
+            'cache' => __DIR__ . '/../../app/cache',
+        ]);
     }
 }
