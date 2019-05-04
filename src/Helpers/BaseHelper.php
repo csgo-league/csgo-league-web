202c6772adcc5d6d625a28048afcf4cadf54f471
B3none <ablackham2000@gmail.com>
2019-05-04 16:44:48 +0100

    fix error

--- src/Helpers/BaseHelper.php
+++ src/Helpers/BaseHelper.php
@@ -28,7 +28,7 @@ class BaseHelper
             $pdo = new \PDO("mysql:host=" . env('DB_HOST') . ";dbname=" . env('DB_NAME'), env('DB_USERNAME'), env('DB_PASSWORD'));
             $this->db = new PDOHandler($pdo);
         } catch (\Exception $e) {
-            die('There was an errors connecting to the database.');
+            die('There was an error connecting to the database.');
         }
     }
 }
