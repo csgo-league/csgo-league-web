213fd32b24c863dd6c9678fc38a944cafd335656
B3none <ablackham2000@gmail.com>
2019-05-06 14:13:13 +0100

    update

--- web/index.php
+++ web/index.php
@@ -16,9 +16,9 @@ if (!function_exists('env')) {
     }
 }
 
-require(__DIR__ . "/../vendor/autoload.php");
-require(__DIR__ . "/../vendor/pecee/simple-router/helpers.php");
-require(__DIR__ . "/../app/Router.php");
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/pecee/simple-router/helpers.php');
+require(__DIR__ . '/../app/Router.php');
 
 /**
  * Register routes
