637706bd5be5ae3a0209ef3d8d5e8ed72112c856
Alex Blackham <ablackham@partspedigree.com>
2019-05-11 20:09:05 +0100

    Add steam auth to the base controller

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -2,12 +2,20 @@
 
 namespace B3none\League\Controllers;
 
+use Twig\Environment;
+use Vikas5914\SteamAuth;
+
 class BaseController
 {
     /**
-     * @var \Twig\Environment
+     * @var Environment
      */
     protected $twig;
+
+    /**
+     * @var SteamAuth
+     */
+    protected $steam;
 
     /**
      * BaseController constructor.
@@ -17,11 +25,19 @@ class BaseController
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../Views');
 
         if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1') {
-            $this->twig = new \Twig\Environment($loader);
+            $this->twig = new Environment($loader);
         } else {
-            $this->twig = new \Twig\Environment($loader, [
+            $this->twig = new Environment($loader, [
                 'cache' => __DIR__ . '/../../app/cache/twig',
             ]);
         }
+
+        $this->steam = new SteamAuth([
+            'apikey' => env('STEAM_API_KEY'), // Steam API KEY
+            'domainname' => 'http://localhost:5000', // Displayed domain in the login-screen
+            'loginpage' => 'http://localhost:5000', // Returns to last page if not set
+            "logoutpage" => '',
+            'skipAPI' => true, // true = dont get the data from steam, just return the steamid64
+        ]);
     }
 }
