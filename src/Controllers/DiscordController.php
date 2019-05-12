2b51a4580ffd7fe4b927e153c16a460f72de48a9
Alex Blackham <ablackham@partspedigree.com>
2019-05-12 17:13:34 +0100

    added code generation

--- src/Controllers/DiscordController.php
+++ src/Controllers/DiscordController.php
@@ -2,8 +2,19 @@
 
 namespace B3none\League\Controllers;
 
+use B3none\League\Helpers\DiscordHelper;
+
 class DiscordController extends BaseController
 {
+    protected $discordHelper;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->discordHelper = new DiscordHelper();
+    }
+
     /**
      * Link Discord
      *
@@ -14,7 +25,36 @@ class DiscordController extends BaseController
     public function linkDiscord(string $discordId, string $token): void
     {
         try {
-            response()->redirect($this->steam->loginUrl());
+            if (!$this->steam->loggedIn()) {
+                response()->redirect($this->steam->loginUrl());
+
+                return;
+            }
+
+            $steamId = $this->authorisedUser['steamid'];
+        } catch (\Exception $e) {
+            header('HTTP/1.1 500 Internal Server Error');
+
+            echo json_encode([
+                'status' => 500
+            ]);
+
+            die;
+        }
+    }
+
+    public function generateDiscordLink(string $discordId)
+    {
+        try {
+            if (!$this->steam->loggedIn()) {
+                response()->redirect($this->steam->loginUrl());
+
+                return;
+            }
+
+            $steamId = $this->authorisedUser['steamid'];
+
+            $this->discordHelper->generateDiscordLinkCode($steamId, $discordId);
         } catch (\Exception $e) {
             header('HTTP/1.1 500 Internal Server Error');
 
--- src/Helpers/DiscordHelper.php
+++ src/Helpers/DiscordHelper.php
@@ -4,9 +4,51 @@ namespace B3none\League\Helpers;
 
 class DiscordHelper extends BaseHelper
 {
-    public function generateDiscordLinkCode(string $steamId, string $memberId): ?string
+    /**
+     * @var CodeHelper
+     */
+    protected $code;
+
+    public function __construct()
     {
+        parent::__construct();
+
+        $this->code = new CodeHelper();
+    }
+
+    public function generateDiscordLinkCode(string $steamId, string $discordId): ?string
+    {
+        $attempts = 0;
+        $length = 5;
+        do {
+            $code = $this->code->generate($length);
+
+            if (++$attempts == 3) {
+                $length += 1;
+                $attempts = 0;
+            }
+        } while ($this->doesCodeExist($code));
+
+        $this->db->insert('player_link_codes', [
+            'steam' => $steamId,
+            'discordId' => $discordId,
+            'code' => $code
+        ]);
 
         return $code;
+    }
+
+    protected function doesCodeExist(string $code): bool
+    {
+        $query = $this->db->query('SELECT * FROM player_link_codes WHERE code = :code', [
+            ':code' => $code
+        ]);
+
+        return $query->rowCount() > 0;
+    }
+
+    protected function checkDiscordLink(string $discordId, string $code)
+    {
+
     }
 }
