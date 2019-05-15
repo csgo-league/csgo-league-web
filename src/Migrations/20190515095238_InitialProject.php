369cae29d56fb51ab523d0b4766120be5d5d3128
Alex Blackham <ablackham@partspedigree.com>
2019-05-15 09:57:41 +0100

    add initial migrations

---
+++ src/Migrations/20190515095238_InitialProject.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace B3none\League\Migrations;
+
+use Phpmig\Migration\Migration;
+
+class InitialProject extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+
+    }
+}
