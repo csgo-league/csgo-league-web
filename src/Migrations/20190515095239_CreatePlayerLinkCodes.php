6dc317fd49fa1e1891215389bc57844c0c67ce24
Alex Blackham <ablackham@partspedigree.com>
2019-05-15 12:29:45 +0100

    add player link codes table

---
+++ src/Migrations/20190515095239_CreatePlayerLinkCodes.php
@@ -0,0 +1,44 @@
+<?php
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+use Medoo\Medoo;
+use Phpmig\Migration\Migration;
+
+class CreatePlayerLinkCodes extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        /**
+         * @var $db Medoo
+         */
+        $db = $this->get('db');
+
+        $query = $db->query('            
+            CREATE TABLE `player_link_codes` (
+              `discord` varchar(100) NOT NULL,
+              `code` varchar(100) NOT NULL,
+              `expires` int(11) DEFAULT NULL
+            );
+        ');
+
+        return $query->execute();
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        /**
+         * @var $db Medoo
+         */
+        $db = $this->get('db');
+
+        $query = $db->query('DROP TABLE IF EXISTS player_link_codes;');
+
+        return $query->execute();
+    }
+}
