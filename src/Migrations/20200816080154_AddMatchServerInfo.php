8f19270afbda74ec4c9b0a30307907d886f4a661
⭐ B3none <ablackham2000@gmail.com>
2020-08-16 10:56:04 +0100

    Add the match server info table columns

---
+++ src/Migrations/20200816080154_AddMatchServerInfo.php
@@ -0,0 +1,44 @@
+<?php
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+use Medoo\Medoo;
+use Phpmig\Migration\Migration;
+
+class AddIsBanned extends Migration
+{
+    /**
+     * Do the migration
+     */
+    public function up()
+    {
+        /**
+         * @var $db Medoo
+         */
+        $db = $this->get('db');
+
+        $query = $db->query('
+            ALTER TABLE matches ADD COLUMN `server_ip` varchar(255);
+            ALTER TABLE matches ADD COLUMN `server_port` int(11) DEFAULT 27015;
+        ');
+
+        return $query->execute();
+    }
+
+    /**
+     * Undo the migration
+     */
+    public function down()
+    {
+        /**
+         * @var $db Medoo
+         */
+        $db = $this->get('db');
+
+        $query = $db->query('
+            ALTER TABLE matches DROP COLUMN `server_ip`;
+            ALTER TABLE matches DROP COLUMN `server_port`;
+        ');
+
+        return $query->execute();
+    }
+}
